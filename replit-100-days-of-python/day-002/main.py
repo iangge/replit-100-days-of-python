2c21edc39767ab7ff26f2d30114e8c8f632f80ed
Ian <87641560+iangge@users.noreply.github.com>
2024-12-16 11:33:11 -0800

    Update main.py for day-002

--- replit-100-days-of-python/day-002/main.py
+++ replit-100-days-of-python/day-002/main.py
@@ -1,1 +1,8 @@
+myName = input("What is your name? ")
+favoriteFood = input("What is your favorite food? ")
+favoriteMusic = input("What is your favorite music? ")
+location = input("Where do you live? ")
 
+print("Hello, " + myName + "!")
+print("Nice to meet you. We should get some " + favoriteFood)
+print("while listening to " + favoriteMusic + " in " + location + ".")
