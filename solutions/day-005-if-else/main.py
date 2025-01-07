9b4d885f9df005c83901d08f2b0e07934b061709
Ian <87641560+iangge@users.noreply.github.com>
2025-01-07 15:36:43 -0800

    Create main.py for day 005
    Topic: If-else statements

---
+++ solutions/day-005-if-else/main.py
@@ -0,0 +1,20 @@
+print("Marvel Movie Character Creator")
+print("--")
+
+hangingAround = input("Do you like 'hanging around'?: ")
+if hangingAround.lower() == "no":
+  print("Then you're not Spider-man")
+else:
+  print("You are Spider-man")
+
+gravellyVoice = input("Do you have a 'gravelly' voice?: ")
+if gravellyVoice.lower() == "no":
+  print("Aww, then you're not Korg")
+else:
+  print("You're Korg")
+
+feelingMarvelous = input("Do you often feel 'Marvelous'?: ")
+if feelingMarvelous.lower() == "no":
+  print("Then you are not Captain Marvel")
+else:
+  print("You are Captain Marvel!")
