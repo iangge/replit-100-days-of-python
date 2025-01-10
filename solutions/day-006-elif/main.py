e82cb3af35a727cbe2984604c93d0bbdc729109f
Ian <87641560+iangge@users.noreply.github.com>
2025-01-09 23:15:40 -0800

    Fix typo on zoe's username variable

--- solutions/day-006-elif/main.py
+++ solutions/day-006-elif/main.py
@@ -1,11 +1,11 @@
 username = input("Username: ")
 password = input("Password: ")
 
-if username == "ian" and password == "password":
+if username == "ian" and password == "ians-password":
   print("Welcome Ian!")
-elif username == "bill" and password == "password":
+elif username == "bill" and password == "bills-password":
   print("Welcome Bill!")
-elif usernmae == "zoe" and password == "password":
+elif username == "zoe" and password == "zoes-password":
   print("Welcome Zoe!")
 else:
   print("Username and password combination unknown. Please try again.")
