1b48022508a53691a3e0cbcbf76519646492dc12
Ian <87641560+iangge@users.noreply.github.com>
2025-01-09 23:13:38 -0800

    Create main.py for Day 6
    Topic: elif statements

---
+++ solutions/day-006-elif/main.py
@@ -0,0 +1,11 @@
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "ian" and password == "password":
+  print("Welcome Ian!")
+elif username == "bill" and password == "password":
+  print("Welcome Bill!")
+elif usernmae == "zoe" and password == "password":
+  print("Welcome Zoe!")
+else:
+  print("Username and password combination unknown. Please try again.")
